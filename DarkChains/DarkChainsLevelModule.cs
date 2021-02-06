9db01f3328c8a7babaddd97242232f861438284e
Lil Husky VR <lilhuskyvr@gmail.com>
2021-02-06 17:04:25 +1100

    save

---
+++ DarkChains/DarkChainsLevelModule.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using HarmonyLib;
+using ThunderRoad;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace DarkChains
+{
+    public class DarkChainsLevelModule : LevelModule
+    {
+        private Harmony _harmony;
+
+        public override IEnumerator OnLoadCoroutine(Level level)
+        {
+            try
+            {
+                _harmony = new Harmony("DarkChains");
+                _harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+                Debug.Log("Dark Chains Loaded");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
+
+            EventManager.onCreatureSpawn += EventManagerOnonCreatureSpawn;
+
+            return base.OnLoadCoroutine(level);
+        }
+
+        private void EventManagerOnonCreatureSpawn(Creature creature)
+        {
+            var ragdoll = creature.ragdoll;
+            foreach (var part in ragdoll.parts)
+            {
+                try
+                {
+                    Object.Destroy(part.gameObject.GetComponent<FrozenRagdollPart>());
+                }
+                catch (Exception exception)
+                {
+                    Debug.Log(exception.Message);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(HandleRagdoll))]
+        [HarmonyPatch("OnGrab")]
+        private static class RagdollHandleOnGrabPatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(HandleRagdoll __instance)
+            {
+                var ragdollPart = __instance.ragdollPart;
+                try
+                {
+                    Object.Destroy(ragdollPart.gameObject.GetComponent<FrozenRagdollPart>());
+                }
+                catch (Exception exception)
+                {
+                    Debug.Log(exception.Message);
+                }
+
+                RagdollPart otherPart = null;
+
+                if (ragdollPart.type == RagdollPart.Type.LeftArm)
+                    otherPart = ragdollPart.ragdoll.GetPart(RagdollPart.Type.LeftHand);
+                if (ragdollPart.type == RagdollPart.Type.RightArm)
+                    otherPart = ragdollPart.ragdoll.GetPart(RagdollPart.Type.RightHand);
+                if (ragdollPart.type == RagdollPart.Type.LeftLeg)
+                    otherPart = ragdollPart.ragdoll.GetPart(RagdollPart.Type.LeftFoot);
+                if (ragdollPart.type == RagdollPart.Type.RightLeg)
+                    otherPart = ragdollPart.ragdoll.GetPart(RagdollPart.Type.RightFoot);
+
+                if (otherPart != null)
+                {
+                    try
+                    {
+                        Object.Destroy(otherPart.gameObject.GetComponent<FrozenRagdollPart>());
+                    }
+                    catch (Exception exception)
+                    {
+                        Debug.Log(exception.Message);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(HandleRagdoll))]
+        [HarmonyPatch("OnTelekinesisGrab")]
+        private static class RagdollHandleOnTelekinesisGrabPatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(SpellTelekinesis spellTelekinesis, HandleRagdoll __instance)
+            {
+                var ragdollPart = __instance.ragdollPart;
+
+                if (spellTelekinesis.spellCaster.spellInstance.id == "DarkChains")
+                {
+                    ragdollPart.characterJoint.breakForce = 3000;
+                }
+                
+                try
+                {
+                    Object.Destroy(ragdollPart.gameObject.GetComponent<FrozenRagdollPart>());
+                }
+                catch (Exception exception)
+                {
+                    Debug.Log(exception.Message);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(HandleRagdoll))]
+        [HarmonyPatch("OnTelekinesisRelease")]
+        private static class RagdollHandleOnTelekinesisReleasePatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(SpellTelekinesis spellTelekinesis, bool tryThrow, ref bool throwing,
+                HandleRagdoll __instance)
+            {
+                var ragdollPart = __instance.ragdollPart;
+
+                if (!throwing && spellTelekinesis.spellCaster.spellInstance.id == "DarkChains")
+                {
+                    if (ragdollPart.gameObject.GetComponent<FrozenRagdollPart>() == null)
+                    {
+                       var frozenRagdollPart =  ragdollPart.gameObject.AddComponent<FrozenRagdollPart>();
+
+                       frozenRagdollPart.Init(__instance);
+                    }
+
+                    try
+                    {
+                        var creature = ragdollPart.ragdoll.creature;
+                        if (creature.gameObject.GetComponent<FrozenCreature>() == null)
+                        {
+                            creature.gameObject.AddComponent<FrozenCreature>();
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        Debug.Log(exception.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DarkChains/FrozenRagdollPart.cs
@@ -0,0 +1,38 @@
+﻿using ThunderRoad;
+using UnityEngine;
+
+namespace DarkChains
+{
+    public class FrozenRagdollPart : MonoBehaviour
+    {
+        private RagdollPart _ragdollPart;
+        private EffectData _ragdollGripEffectData;
+        private EffectInstance _ragdollGripEffect;
+
+        private void Start()
+        {
+            _ragdollPart = GetComponent<RagdollPart>();
+            _ragdollPart.rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+
+        public void Init(HandleRagdoll handleRagdoll)
+        {
+            _ragdollGripEffectData = Catalog.GetData<EffectData>("SpellGravityGrab");
+            _ragdollGripEffect = _ragdollGripEffectData.Spawn(handleRagdoll.transform);
+            _ragdollGripEffect.Play();
+            foreach (var effect in _ragdollGripEffect.effects)
+            {
+                if (effect is EffectAudio)
+                {
+                    effect.End();
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _ragdollPart.rb.constraints = RigidbodyConstraints.None;
+            _ragdollGripEffect.End();
+        }
+    }
+}
